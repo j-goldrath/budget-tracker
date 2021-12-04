fe73864a0db5266758a2a6f54420d46d4498e665
j-goldrath <jonathan.goldrath@gmail.com>
2021-12-04 00:38:43 -0600

    fixed problems with paths in service worker and moved serviceworker.js and manifest.json to public directory

--- public/service-worker.js
+++ public/service-worker.js
@@ -3,14 +3,14 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('static').then(cache => {
       return cache.addAll([
-        './',
-        './manifest.json',
-        './public/index.html',
-        './public/index.js',
-        './public/indexedDb.js',
-        './public/css/style.css',
-        './public/icons/icon-192x192.png',
-        './public/icons/icon-192x192.png',
+        '/',
+        '/manifest.json',
+        '/index.html',
+        '/index.js',
+        '/indexedDb.js',
+        '/styles.css',
+        '/icons/icon-192x192.png',
+        '/icons/icon-512x512.png',
         'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
         'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
       ]);
