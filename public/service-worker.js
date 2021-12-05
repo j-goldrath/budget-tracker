2737ad21c9b6be4041064d6185fc2d626bdbde7f
j-goldrath <jonathan.goldrath@gmail.com>
2021-12-04 18:11:30 -0600

    added tto event handler code for caching behavior

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,3 +1,6 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 const FILES_TO_CACHE = [
   '/',
   '/manifest.webmanifest',
@@ -11,12 +14,10 @@ const FILES_TO_CACHE = [
   'https://cdn.jsdelivr.net/npm/chart.js@2.8.0'
 ]
 
-
-
 // install event handler
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open('static').then(cache => {
+    caches.open('DATA_CACHE_NAME').then(cache => {
       return cache.addAll(FILES_TO_CACHE);
     })
   );
@@ -24,13 +25,48 @@ self.addEventListener('install', event => {
   self.skipWaiting();
 });
 
-// retrieve assets from cache
+// activate event handler
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('Removing old cache data', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+// retrieve assets from cache when fetch event is triggered
 self.addEventListener('fetch', event => {
-  if (event.request.url.includes("/api/")) {
+  if (event.request.url.includes('/api/')) {
     event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
+      caches.open(DATA_CACHE_NAME).then(cache => {
+        return fetch(event.request)
+          .then(response => {
+            // If the response was good, clone it and store it in the cache.
+            if (response.status === 200) {
+              cache.put(event.request.url, response.clone());
+            }
+            return response;
+          })
+          .catch(err => {
+            // Network request failed, try to get it from the cache.
+            return cache.match(event.request);
+          });
+      }).catch(err => console.log(err))
     );
+    return;
   }
+
+  event.respondWith(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+    );
 });
